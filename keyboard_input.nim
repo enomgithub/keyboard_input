c2c52a73a029dba179ce51d43b9e24af2d1bc2a6
enom <satoshi.enom@gmail.com>
2017-12-26 09:46:14 +0900

    Modify: Refactoring

--- keyboard_input.nim
+++ keyboard_input.nim
@@ -3,25 +3,21 @@ from strutils import intToStr, parseInt
 
 
 proc incWidth(element: Element) =
-  let width = parseInt(element.style.width)
-  if width >= window.innerWidth:
-    discard
-  else:
-    element.style.width = intToStr(width + 1) & "px"
+  let width = element.style.width.parseInt
+  if width >= window.innerWidth: discard
+  else: element.style.width = (width + 1).intToStr & "px"
 
 
 proc decWidth(element: Element) =
-  let width = parseInt(element.style.width)
-  if width <= 1:
-    discard
-  else:
-    element.style.width = intToStr(width - 1) & "px"
+  let width = element.style.width.parseInt
+  if width <= 1: discard
+  else: element.style.width = (width - 1).intToStr & "px"
 
 
 proc toggleKey(keyCode: int, element: Element) =
   const
-    jKey: int = 74
-    kKey: int = 75
+    jKey: int8 = int8('J')  # = 74'i8
+    kKey: int8 = int8('K')  # = 75'i8
   case keyCode
   of jKey: decWidth(element)
   of kKey: incWidth(element)
@@ -30,17 +26,18 @@ proc toggleKey(keyCode: int, element: Element) =
 
 proc main() =
   const
-    id = "bar"
-    height = 10
-    width = 100
-    color = "#302833"
-  let bar = document.getElementById(id)
-  bar.style.backgroundColor = color
-  bar.style.height = intToStr(height) & "px"
-  bar.style.width = intToStr(width) & "px"
-  window.addEventListener("keydown",
-                          proc(e: Event) = toggleKey(e.keyCode, bar),
-                          false)
+    ID = "bar"
+    HEIGHT = 10
+    WIDTH = 100
+    COLOR = "#302833"
+  let bar = document.getElementById(ID)
+  bar.style.backgroundColor = COLOR
+  bar.style.height = HEIGHT.intToStr & "px"
+  bar.style.width = WIDTH.intToStr & "px"
+  window.addEventListener( "keydown"
+                         , proc(e: Event) = toggleKey(e.keyCode, bar)
+                         , false
+                         )
 
 
 when isMainModule:
